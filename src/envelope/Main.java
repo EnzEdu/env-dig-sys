96334d25f65487f6a7296f319e4c95d8db2227cd
EnzEdu <enzinhovlogs@outlook.com>
2023-06-28 08:45:23 -0300

    Adicionada classe Tela

---
+++ src/envelope/Main.java
@@ -0,0 +1,9 @@
+package envelope;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Tela.criaTela();
+	}
+
+}
---
+++ src/envelope/Tela.java
@@ -0,0 +1,8 @@
+package envelope;
+
+public class Tela {
+
+	public static void criaTela() {
+		System.out.println("placeholder");
+	}
+}
